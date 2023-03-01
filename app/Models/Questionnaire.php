231303bad9d6129adc93c7dbec41bd7be7add620
Lakshan Madushanka <epmadushanka@gmail.com>
2023-03-01 11:18:17 +0530

    expires_at column datetime cast

--- app/Models/Questionnaire.php
+++ app/Models/Questionnaire.php
@@ -92,6 +92,7 @@ class Questionnaire extends Model
     protected $casts = [
         'difficulty' => Difficulty::class,
         'single_answers_type' => 'boolean',
+        'expires_at' => 'datetime'
     ];
 
     // --------------------------------Scopes--------------------------------------------
