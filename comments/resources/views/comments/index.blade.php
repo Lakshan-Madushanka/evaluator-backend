d52ab6da357b4ce34d452e30aadca12580c4166b
Lakshan Madushanka <epmadushanka@gmail.com>
2025-08-02 08:59:38 +0530

    remove: comments directory

--- comments/resources/views/comments/index.blade.php
+++
@@ -1,9 +0,0 @@
-@php use App\Models\Post;use Illuminate\Support\Str; @endphp
-
-<x-app-layout>
-{{--    <x-comments::form  modelClass="{{Post::class}}" modelId="{{$post->getKey()}}"/>--}}
-    <br><br><br>
-    <div class="m-4">
-        <x-comments::index :model="$post" />
-    </div>
-</x-app-layout>
